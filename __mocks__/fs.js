b9f7e87cce6cd45595074c7937a28be5793e832d
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-10-17 08:03:52 -0500

    read tests

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -5,9 +5,23 @@
 const fs = jest.createMockFromModule('fs')
 
 const files = require('../test-lib/files')
-const { Readable } = require('stream')
+const tick = require('../test-lib/tick')
+const { Readable, PassThrough } = require('stream')
+
+fs._readStreamErrorFiles = new Set()
+
+fs._reset = () => {
+  fs._readStreamErrorFiles.clear()
+}
 
 fs.createReadStream = path => {
+  if (fs._readStreamErrorFiles.has(path)) {
+    const stream = new PassThrough()
+    tick().then(() => {
+      stream.destroy(new Error('Error creating a read stream.'))
+    })
+    return stream
+  }
   if (files.has(path)) {
     return Readable.from(files.get(path).split(''))
   } else {
