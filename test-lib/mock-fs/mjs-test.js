46bd23958005759d9156d0ed947caee73a1e60ec
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-10-19 16:28:53 -0500

    Test fs mock

--- test-lib/mock-fs/mjs-test.js
+++ test-lib/mock-fs/mjs-test.js
@@ -1,8 +1,23 @@
-/* global jest, test */
+/* global jest, test, expect */
+
+import { createReadStream, _readStreamErrorFiles, _reset } from 'fs'
+import files from '../files/index.js'
+import chunks from '../chunks/index.js'
 
 jest.mock('fs')
 
-// TODO: actual tests
-test('test', () => {
+test('createReadStream', async () => {
+  files.set('file', 'hi')
+  expect(await chunks(createReadStream('file'))).toStrictEqual(['h', 'i'])
+})
 
+test('_readStreamErrorFiles', async () => {
+  _readStreamErrorFiles.add('file')
+  await expect(chunks(createReadStream('file'))).rejects.toMatchSnapshot()
+})
+
+test('_reset', () => {
+  _readStreamErrorFiles.add('file')
+  _reset()
+  expect(_readStreamErrorFiles.size).toBe(0)
 })
