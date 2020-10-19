38121eb075037e9afcc7d41df6a2c4da4dce6fe1
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-10-18 19:13:27 -0500

    test no-resolve.js

--- test-lib/no-resolve.js
+++ test-lib/no-resolve.js
@@ -1,5 +1,3 @@
-// TODO: add tests to this file
-
 const noResolve = (promise, timeout) => new Promise((resolve, reject) => {
   promise.then(() => {
     reject(new Error('Given promise was resolved.'))
