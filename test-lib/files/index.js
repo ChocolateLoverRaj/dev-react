5d8ac55f7819373a6539279af097c6c7d4b6b161
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-10-21 10:04:49 -0500

    File tree

--- test-lib/files/index.js
+++ test-lib/files/index.js
@@ -1,4 +1,40 @@
-// TODO: structure with folders and files
-const topDir = new Map()
+export class File {
+  canRead = true
+  canWrite = true
+}
 
-export default topDir
+export class NormalFile extends File {
+  constructor (content) {
+    super()
+    this.content = content
+  }
+}
+
+export class Dir extends File {
+  files = new Map()
+}
+
+class EnoentError extends Error {
+  code = 'ENOENT'
+}
+
+export const topDir = new Dir()
+
+export const reset = () => {
+  topDir.files.clear()
+}
+
+export const getFile = path => {
+  let file = topDir
+  const filenames = path.split('/')
+  let fileTree = []
+  for (const filename of filenames) {
+    fileTree.push(filename)
+    if (file.files.has(filename)) {
+      file = file.files.get(filename)
+    } else {
+      throw new EnoentError(`File: ${fileTree.join('/')} does not exist`)
+    }
+  }
+  return file
+}
--- test-lib/files/index.test.js
+++ test-lib/files/index.test.js
@@ -1,7 +1,86 @@
-/* global test, expect */
+/* global afterEach, describe, test, expect */
 
-import files from './index.js'
+import { File, NormalFile, Dir, topDir, getFile, reset } from './index.js'
 
-test('is a Map', () => {
-  expect(Object.getPrototypeOf(files)).toBe(Map.prototype)
+afterEach(reset)
+
+describe('inheritance', () => {
+  test('File', () => {
+    const file = new File()
+    expect(file).toBeInstanceOf(File)
+    expect(file).not.toBeInstanceOf(NormalFile)
+    expect(file).not.toBeInstanceOf(Dir)
+  })
+
+  test('NormalFile', () => {
+    const file = new NormalFile('content')
+    expect(file).toBeInstanceOf(File)
+    expect(file).toBeInstanceOf(NormalFile)
+    expect(file).not.toBeInstanceOf(Dir)
+  })
+
+  test('Dir', () => {
+    const file = new Dir()
+    expect(file).toBeInstanceOf(File)
+    expect(file).not.toBeInstanceOf(NormalFile)
+    expect(file).toBeInstanceOf(Dir)
+  })
+})
+
+const testFiles = (createFile) => {
+  function topLevel () {
+    const file = createFile()
+    topDir.files.set('file', file)
+    expect(getFile('file')).toBe(file)
+  }
+  function subDir () {
+    const file = createFile()
+    const dir = new Dir()
+    dir.files.set('file', file)
+    topDir.files.set('dir', dir)
+    expect(getFile('dir/file')).toBe(file)
+  }
+  function subSubDir () {
+    const file = createFile()
+    const subDir = new Dir()
+    subDir.files.set('file', file)
+    const dir = new Dir()
+    dir.files.set('subDir', subDir)
+    topDir.files.set('dir', dir)
+    expect(getFile('dir/subDir/file')).toBe(file)
+  }
+  topLevel()
+  subDir()
+  subSubDir()
+}
+
+describe('getFile', () => {
+  describe('nonexistent file', () => {
+    test('in topDir', () => {
+      expect(() => {
+        getFile('file')
+      }).toThrowErrorMatchingSnapshot()
+    })
+
+    test('in subDir', () => {
+      topDir.files.set('dir', new Dir())
+      expect(() => {
+        getFile('dir/file')
+      }).toThrowErrorMatchingSnapshot()
+    })
+
+    test('in nonexistent subDir', () => {
+      expect(() => {
+        getFile('dir/file')
+      }).toThrowErrorMatchingSnapshot()
+    })
+  })
+
+  test('get NormalFile', () => {
+    testFiles(() => new NormalFile('hi'))
+  })
+
+  test('get Dir', () => {
+    testFiles(() => new Dir())
+  })
 })
