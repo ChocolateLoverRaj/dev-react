36147f43a32f09ba15db25c816ad6d796876032b
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-11-06 12:53:34 -0600

    Added typescript definitions

---
+++ lib/refresher.d.ts
@@ -0,0 +1,11 @@
+type promiseFunc = () => Promise<unknown>
+
+declare class Refresher {
+  constructor()
+
+  running: boolean
+  next?: promiseFunc
+
+  run(func: promiseFunc): void
+  refresh(func: promiseFunc): void
+}
