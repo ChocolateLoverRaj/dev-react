09cb531c553518c42c7713c22e8c6fed992c57be
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-11-08 10:14:26 -0600

    Async DisplayTask.close()

--- lib/display.d.ts
+++ lib/display.d.ts
@@ -5,8 +5,13 @@ type lines = Array<string>
 declare class Display {
   refresher: Refresher
   lines: lines
+  cursorX: number
+  cursorY: number
 
-  update(lines: lines): void
+  moveCursorX(x: number): Promise<void>
+  moveCursorY(y: number): Promise<void>
+  update(lines: lines): Promise<void>
+  close(): Promise<void>
 }
 
 export default Display
