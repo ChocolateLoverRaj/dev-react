c0f8bde9c23d8d2a44fb5638a8dac9cee5eaa06c
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-11-09 10:33:46 -0600

    Basic tasks and addons

--- lib/tasks/progress-addon.d.ts
+++
@@ -1,9 +0,0 @@
-import BaseAddon from './base-addon'
-import Styles from './default-styles'
-
-declare class ProgressAddon extends BaseAddon {
-  progress: number
-  styles: typeof Styles
-}
-
-export default ProgressAddon
