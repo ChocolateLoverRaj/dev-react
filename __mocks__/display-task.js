4fb1c8d92b2b278d3eb6454ce28af447072d9ce7
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-01 11:58:19 -0600

    Fix lint

--- __mocks__/display-task.js
+++ __mocks__/display-task.js
@@ -1,2 +1,22 @@
 export { default as DisplayTask } from '../test-lib/mock-display-task/display-task.js'
-export * from 'display-task'
+export { default } from '../test-lib/mock-display-task/display-task.js'
+export {
+  Addon,
+  Addons,
+  AutoStateAddon,
+  ColorAddon,
+  ConcatAddon,
+  ConcatTask,
+  CustomTask,
+  HideTask,
+  MapTask,
+  MaxLengthAddon,
+  NameTask,
+  SetAddon,
+  SetTask,
+  SortSetTask,
+  StateAddon,
+  Task,
+  Tasks,
+  TextAddon
+} from 'display-task'
